ca91aac6d8b49f491db4099906953f3523410255
PiKeeb <pikeeb@gmail.com>
2021-11-17 13:24:19 +0300

    Changed the device descriptor parameters

--- keyboards/6x16/config.h
+++ keyboards/6x16/config.h
@@ -20,9 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
-#define DEVICE_VER   0x0001
+#define VENDOR_ID    0x494c // IL for ILWrites
+#define PRODUCT_ID   0x0616 // 0616 for 6x16
+#define DEVICE_VER   0x0001 // First version
 #define MANUFACTURER ILWrites
 #define PRODUCT      6x16
 
