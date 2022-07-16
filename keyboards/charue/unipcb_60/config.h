20d6ef3ec16f492849756e6e4b835c425cdb7afb
PiKeeb <pikeeb@gmail.com>
2022-07-16 16:21:45 +0300

    Basic firmware

--- keyboards/charue/unipcb_60/config.h
+++ keyboards/charue/unipcb_60/config.h
@@ -1,20 +1,52 @@
-// Copyright 2022 ILWrites (@PiKeeb)
+// Copyright 2022 Charue Design
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
+#include "config_common.h"
 
-/* disable debug print */
-//#define NO_DEBUG
+/* USB Device descriptor parameter */
+// 0x4344 = CD for Charue Design
+#define VENDOR_ID    0x4344
+// 0x4231 = B1 for Board 1 (uniPCB board 1)
+#define PRODUCT_ID   0x4231
+// 0x0001 = 1 for Base version
+#define DEVICE_VER   0x0001
+#define MANUFACTURER Charue Design
+#define PRODUCT      uniPCB 60
 
-/* disable print */
-//#define NO_PRINT
+/* Key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
 
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+/* Keyboard Matrix Assignments*/
+#define MATRIX_ROW_PINS { F1, F0, B0, C7, D5 }
+#define MATRIX_COL_PINS { B6, B5, B4, D7, D6, D4, D1, D0, F7, F6, F5, C6, D2, B3 }
+#define UNUSED_PINS { B7, E6, F4 }
+
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* RGB */
+#define RGB_DI_PIN D3
+#ifdef RGB_DI_PIN
+#    define RGBLED_NUM 10
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_RGB_TEST
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_TWINKLE
+#endif
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
---
+++ keyboards/charue/unipcb_65/keymaps/debug/readme.md
@@ -0,0 +1,1 @@
+# The debug keymap for uniPCB 65%
