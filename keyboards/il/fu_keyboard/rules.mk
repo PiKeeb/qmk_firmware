ca74544825980b6946513c578db8f3461532ec66
PiKeeb <pikeeb@gmail.com>
2023-05-25 13:45:51 +0300

    Test firmware v0.1

---
+++ keyboards/il/fu_keyboard/rules.mk
@@ -0,0 +1,13 @@
+VIA_ENABLE = yes
+OLED_ENABLE = yes
+AUDIO_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+# Audio driver
+AUDIO_DRIVER = pwm_hardware
+
+# OLED Driver
+OLED_DRIVER = SSD1306
+
+# Enable Link Time Optimization
+LTO_ENABLE = yes
